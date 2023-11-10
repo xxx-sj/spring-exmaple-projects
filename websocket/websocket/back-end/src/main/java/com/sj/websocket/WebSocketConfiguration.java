381bdb61b417f6e33d31105018b3c0bf78f56196
loky13 <loky13@daum.net>
2023-11-11 01:31:47 +0900

    websocket 구현

---
+++ websocket/websocket/back-end/src/main/java/com/sj/websocket/WebSocketConfiguration.java
@@ -0,0 +1,23 @@
+package com.sj.websocket;
+
+import com.sj.websocket.handler.MyHandler;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+@Configuration
+@EnableWebSocket
+public class WebSocketConfiguration implements WebSocketConfigurer {
+    @Override
+    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+        registry.addHandler(myHandler(), "/myHandler").setAllowedOriginPatterns("*");
+    }
+
+    @Bean
+    public WebSocketHandler myHandler() {
+        return new MyHandler();
+    }
+}
