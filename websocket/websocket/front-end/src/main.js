1707a13a65b299a2e45e93f448a27f0b26cebdde
loky13 <loky13@daum.net>
2023-11-12 02:14:14 +0900

    base spinner 추가

--- websocket/websocket/front-end/src/main.js
+++ websocket/websocket/front-end/src/main.js
@@ -1,14 +1,15 @@
 import { createApp } from 'vue'
+import BaseSpinner from '@/components/UI/BaseSpinner'
 
 // import router from '@/router'
 import store from "@/store"
 import App from './App.vue'
 
-createApp(App).mount('#app')
-
 const app = createApp(App);
 
 // app.use(router)
 app.use(store)
+
+app.component('base-spinner', BaseSpinner)
 
 app.mount("#app")
