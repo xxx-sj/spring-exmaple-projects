a92fd906537d38f919fc30bd3fef9caf0d734cd8
loky13 <loky13@daum.net>
2023-11-11 23:49:39 +0900

    store template 추가

---
+++ websocket/websocket/front-end/src/store/index.js
@@ -0,0 +1,9 @@
+import { createStore } from "vuex";
+import userModule from "./modules/user"
+const store = createStore({
+    modules: {
+        user: userModule
+    }
+});
+
+export default store;
