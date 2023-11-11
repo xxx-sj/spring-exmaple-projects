4b7c94c253bcfbb17d639e23d88501923988626a
loky13 <loky13@daum.net>
2023-11-12 02:15:30 +0900

    dev option 추가

--- websocket/websocket/front-end/vue.config.js
+++ websocket/websocket/front-end/vue.config.js
@@ -1,4 +1,27 @@
 const { defineConfig } = require('@vue/cli-service')
+
+const host = "localhost";
+const port = "8081";
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+
+  outputDir: "../back-end/src/main/resources/static",
+
+
+  devServer: {
+    hot: true,
+    proxy: {
+      //http://localhost:8081/api/ -> http://localhost:8080/api/
+      '/api/': {
+        target: `http://${host}:${port}`,
+        changeOrigin: true,
+      },
+
+      '/ws/': {
+        target: `ws://${host}:${port}`,
+        changeOrigin: false,
+        ws: true,
+      }
+    }
+  }
 })
