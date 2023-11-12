0307e2a88c14cbf8ae85aa593b0f374d742baf8b
loky13 <loky13@daum.net>
2023-11-12 18:42:21 +0900

    eslint 수정

--- websocket/websocket/front-end/.eslintrc.js
+++ websocket/websocket/front-end/.eslintrc.js
@@ -11,7 +11,9 @@ module.exports = {
     parser: '@babel/eslint-parser'
   },
   rules: {
+    'no-unused-vars': "off",
+    "no-dupe-keys": "off",
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
   }
 }
